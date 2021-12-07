6d466c0b43bfdb47bdbbcf2067d5cce1a7bf601f
Daniel Ranchal <daniranchal@wealize.digital>
2021-12-07 16:14:51 +0100

    Final updates

--- anomalies/OutliersPaquetes.R
+++ anomalies/OutliersPaquetes.R
@@ -1,4 +1,4 @@
-# Máster -> Detección de anomalías
+# M?ster -> Detecci?n de anomal?as
 # Juan Carlos Cubero. Universidad de Granada
 
 install.packages("devtools")
@@ -6,20 +6,20 @@ library(devtools)
 install.packages("ggplot2")
 
 # ggbiplot:
-install_github("vqv/ggbiplot")  # Si pregunta, seleccione la opción 3.
+install_github("vqv/ggbiplot")  # Si pregunta, seleccione la opci?n 3.
 
-# El directorio en el que se instale no debe tener caracteres como acentos, ñ, etc.
+# El directorio en el que se instale no debe tener caracteres como acentos, ?, etc.
 # 
 # Si no funciona lo anterior, probar alguna de las siguientes:
 # devtools::install_github("richardjtelford/ggbiplot", ref = "experimental")
 # install_github("ggbiplot", "vqv")
 # install_github("vqv/ggbiplot", ref = "experimental")
 
-install.packages("fitdistrplus")  # Ajuste de una distribución
+install.packages("fitdistrplus")  # Ajuste de una distribuci?n
 install.packages("reshape")       # melt
 install.packages("tidyverse")
 install.packages("outliers")      # Grubbs
-install.packages("CerioliOutlierDetection")  #MCD Hardin Rocke estimación robusta de la matriz de covarianzas
+install.packages("CerioliOutlierDetection")  #MCD Hardin Rocke estimaci?n robusta de la matriz de covarianzas
 install.packages("mvoutlier")     # corr.plot , MCD ChiC 
 install.packages("mvnormtest")    # mvn. Test Normalidad multivariante
 install.packages("MVN")           
