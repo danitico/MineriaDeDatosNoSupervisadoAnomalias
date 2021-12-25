e1bd0c4ac6aa92defb01a3c1aad46b74aa2f901e
Daniel Ranchal Parrado <danielranchal@correo.ugr.es>
2021-12-25 20:47:54 +0100

    Making some changes

--- final_project/main.R
+++ final_project/main.R
@@ -15,118 +15,108 @@ df <- df %>% mutate(
         Handicapped_infants:Export_south_africa,
         ~ factor(.x, levels = c("y", "n", "NA"), labels = c("Sí", "No", "Abstención"))
     )
-)
+) %>% rename(Party_affiliation=Class)
 
-# handicapped infants -> https://www.congress.gov/bill/98th-congress/house-bill/808
-# water project cost -> https://scorecard.lcv.org/roll-call-vote/1984-540-water-project-cost-sharing-maintenance-and-repairs
-# adoption budget resolution -> https://www.congress.gov/bill/98th-congress/house-bill/5247
-# physician fee freeze -> https://www.healthaffairs.org/doi/10.1377/hlthaff.8.1.21
-# el salvador aid -> https://www.washingtonpost.com/archive/politics/1984/05/11/reagan-wins-narrowly-on-aid-to-el-salvador/6ed8ac74-99b1-4fcc-8d9d-c57ba1295891/
-# religious groups -> https://www.nytimes.com/1984/05/16/us/bill-for-religion-in-public-schools-loses-house-vote.html
-# anti-satellite test ban -> https://www.nytimes.com/1985/12/17/world/pentagon-assails-proposed-anti-satellite-test-ban.html
-# aid to nicaraguan contras -> https://www.latimes.com/archives/la-xpm-1985-06-13-mn-10854-story.html
-# mx missile -> https://www.nytimes.com/1984/05/17/us/compromise-on-mx-is-passed-by-house-missile-total-cut.html
-# immigration -> https://www.nytimes.com/1984/06/21/us/house-by-216-211-approves-aliens-bill-after-retaining-amnesty-plan-in-final-test.html
-# synfuel corporation cutback -> https://www.nytimes.com/1984/08/03/business/house-votes-to-tighten-synthetic-fuels-money.html
-# education spending -> https://www.edweek.org/education/congress-set-to-pass-1984-e-d-budget/1983/10
-# right to sue -> https://library.cqpress.com/cqalmanac/document.php?id=cqal84-1152958
-# crime -> https://en.wikipedia.org/wiki/Comprehensive_Crime_Control_Act_of_1984
-# duty free exports -> https://www.nytimes.com/1984/03/28/business/us-cuts-list-of-duty-free-imports.html
-# export south africa -> https://www.congress.gov/bill/98th-congress/house-bill/4230?s=1&r=92
-
-dfTransactions <- as(df, "transactions")
-
-summary(dfTransactions)
-image(dfTransactions)
-
-itemFrequencyPlot(dfTransactions, support = 0.1, cex.names = 0.8)
-
-
-aprioriItemsets <- apriori(dfTransactions, parameter = list(support = 0.15, target = "frequent"))
-aprioriItemsets <- sort(aprioriItemsets, by="support")
-inspect(head(aprioriItemsets, n=10))
-
-rules <- apriori(dfTransactions, parameter = list(support = 0.2, confidence = 0.8, minlen=2, maxlen=4))
-summary(rules)
-
-# meow1 <- subset(sortedRules, rhs %in% "stabilityCoreTemperature=unstable")
-# summary(meow1)
-# inspect(head(meow1))
-
-redundant <- is.redundant(rules, measure = "confidence")
-rulesPruned <- rules[!redundant]
-summary(rulesPruned)
-
-conviction <- interestMeasure(
-    rulesPruned,
-    measure=c("conviction"),
-    transactions=dfTransactions
-)
-
-quality(rulesPruned) <- cbind(quality(rulesPruned), conviction)
-
-interestingRules <- subset(rulesPruned, conviction > 1 & conviction <= 5)
-summary(interestingRules)
-sortedRulesbyConviction <- sort(interestingRules, by="conviction")
-inspect(head(sortedRulesbyConviction, n=20))
-
-
-sortedRulesBySupport <- sort(rulesPruned, by="support")
-inspect(head(sortedRulesBySupport, n=10))
-
-meow1 <- subset(rulesPruned, rhs %in% "Class=republican")
-summary(meow1)
-inspect(head(sort(meow1, by="conviction"), n=10))
-
-meow2 <- subset(rulesPruned, rhs %in% "Mx_missile=No" & lhs %in% "Anti_satellite_test_ban=Sí")
-summary(meow2)    
-inspect(head(meow2))
-
-
-
-
-dfNegativeItems <- df
-oneHotCodification <- dummyVars(~ Export_south_africa, data=dfNegativeItems)
+oneHotCodification <- dummyVars(~ Export_south_africa + Water_project_cost, data=df)
 conversion <- data.frame(predict(oneHotCodification, newdata=df)) %>% mutate(
     across(
-        1:3,
+        1:6,
         ~ factor(.x, levels = c(0, 1), labels = c(FALSE, TRUE))
     )
 )
 
-negativeItemsNames <- names(conversion)
+dfWithNegativeItems <- df
+dfWithNegativeItems$Export_south_africa <- NULL
+dfWithNegativeItems$Water_project_cost <- NULL
+dfWithNegativeItems <- cbind(dfWithNegativeItems, conversion)
 
-dfNegativeItems$Export_south_africa <- NULL
-dfNegativeItems <- cbind(dfNegativeItems, conversion)
+dfTransactions <- as(dfWithNegativeItems, "transactions")
+summary(dfTransactions)
+image(dfTransactions)
 
-dfTransactionsNegativeItems <- as(dfNegativeItems, "transactions")
 
-itemFrequencyPlot(dfTransactionsNegativeItems, support = 0.2, cex.names = 0.8)
-rules1 <- apriori(dfTransactionsNegativeItems, parameter = list(support = 0.2, confidence = 0.8, minlen=2))
-summary(rules1)
+itemFrequencyPlot(dfTransactions, support = 0.2, cex.names = 0.8)
+rules <- apriori(dfTransactions, parameter = list(support = 0.2, confidence = 0.8, minlen=2, maxlen=4))
+summary(rules)
 
-redundant1 <- is.redundant(rules1, measure = "confidence")
-rulesPruned1 <- rules1[!redundant1]
-summary(rulesPruned1)
+redundant <- is.redundant(rules, measure = "confidence")
+rules <- rules[!redundant]
+summary(rules)
 
-inspect(head(rulesPruned1))
+# Clean redundant rules regarding negative items
 
 # No existen reglas redundantes
-a <- subset(rulesPruned1, lhs %in% "Export_south_africa.Abstención=TRUE" & lhs %in% "Export_south_africa.Sí=FALSE")
+a <- subset(rules, lhs %in% "Export_south_africa.Abstención=TRUE" & lhs %in% "Export_south_africa.Sí=FALSE")
 summary(a)
-b <- subset(rulesPruned1, lhs %in% "Export_south_africa.Abstención=TRUE" & lhs %in% "Export_south_africa.No=FALSE")
+b <- subset(rules, lhs %in% "Export_south_africa.Abstención=TRUE" & lhs %in% "Export_south_africa.No=FALSE")
 summary(b)
-c <- subset(rulesPruned1, lhs %in% "Export_south_africa.Sí=TRUE" & lhs %in% "Export_south_africa.Abstención=FALSE")
+c <- subset(rules, lhs %in% "Export_south_africa.Sí=TRUE" & lhs %in% "Export_south_africa.Abstención=FALSE")
 summary(c)
-d <- subset(rulesPruned1, lhs %in% "Export_south_africa.Sí=TRUE" & lhs %in% "Export_south_africa.No=FALSE")
+d <- subset(rules, lhs %in% "Export_south_africa.Sí=TRUE" & lhs %in% "Export_south_africa.No=FALSE")
 summary(d)
-e <- subset(rulesPruned1, lhs %in% "Export_south_africa.No=TRUE" & lhs %in% "Export_south_africa.Sí=FALSE")
+e <- subset(rules, lhs %in% "Export_south_africa.No=TRUE" & lhs %in% "Export_south_africa.Sí=FALSE")
 summary(e)
-f <- subset(rulesPruned1, lhs %in% "Export_south_africa.No=TRUE" & lhs %in% "Export_south_africa.Abstención=FALSE")
+f <- subset(rules, lhs %in% "Export_south_africa.No=TRUE" & lhs %in% "Export_south_africa.Abstención=FALSE")
 summary(f)
+g <- subset(rules, lhs %in% "Water_project_cost.Abstención=TRUE" & lhs %in% "Water_project_cost.Sí=FALSE")
+summary(g)
+h <- subset(rules, lhs %in% "Water_project_cost.Abstención=TRUE" & lhs %in% "Water_project_cost.No=FALSE")
+summary(h)
+i <- subset(rules, lhs %in% "Water_project_cost.Sí=TRUE" & lhs %in% "Water_project_cost.Abstención=FALSE")
+summary(i)
+j <- subset(rules, lhs %in% "Water_project_cost.Sí=TRUE" & lhs %in% "Water_project_cost.No=FALSE")
+summary(j)
+k <- subset(rules, lhs %in% "Water_project_cost.No=TRUE" & lhs %in% "Water_project_cost.Sí=FALSE")
+summary(k)
+l <- subset(rules, lhs %in% "Water_project_cost.No=TRUE" & lhs %in% "Water_project_cost.Abstención=FALSE")
+summary(l)
 
 
-#rkeel
+conviction <- interestMeasure(
+    rules,
+    measure=c("conviction"),
+    transactions=dfTransactions
+)
+
+quality(rules) <- cbind(quality(rules), conviction)
+summary(rules)
+
+interestingRules <- subset(rules, conviction >= 1.01 & conviction <= 5)
+summary(interestingRules)
+inspect(head(interestingRules, n=50))
+
+sortedRulesbyConviction <- sort(interestingRules, by="conviction")
+inspect(head(sortedRulesbyConviction))
+
+sortedRulesBySupport <- sort(interestingRules, by="lift")
+inspect(head(sortedRulesBySupport, n=200))
+
+prueba <- subset(sortedRulesbyConviction, rhs %in% "Party_affiliation=republican")
+summary(prueba)
+inspect(head(prueba))
+
+prueba1 <- subset(sortedRulesbyConviction, rhs %in% "Party_affiliation=democrat")
+summary(prueba1)
+inspect(prueba1)
+
+prueba2 <- subset(sortedRulesbyConviction, lhs %in% "Duty_free_exports=Sí")
+summary(prueba2)
+
+prueba3 <- subset(interestingRules, lhs %in% "Mx_missile=Sí" & rhs %in% "Anti_satellite_test_ban=Sí")
+summary(prueba3)
+inspect(prueba3)
+
+prueba4 <- subset(interestingRules, lhs %in% "Anti_satellite_test_ban=No" & rhs %in% "Mx_missile=No")
+summary(prueba4)
+inspect(prueba4)
+
+aa <- subset(rules, lhs %in% "Anti_satellite_test_ban=No" & lhs %in% "Mx_missile=No" & size(lhs) == 2)
+inspect(aa)
+
+ab <- subset(rules, lhs %in% "Anti_satellite_test_ban=Sí" & lhs %in% "Mx_missile=Sí" & size(lhs) == 2)
+inspect(ab)
+
+#rkeel MOPNAR
 
 mopnar <- MOPNAR_A(df)
 mopnar$run()
@@ -138,8 +128,8 @@ mopnarRulesRedundant <- is.redundant(mopnar_rules, measure = "confidence")
 mopnar_rules <- mopnar_rules[!mopnarRulesRedundant]
 summary(mopnar_rules)
 
-sorted_mopnar_rules <- sort(mopnar_rules, by="support")
-inspect(head(sorted_mopnar_rules))
+
+inspect(mopnar_rules)
 
 
 
